d083ef68a16c823b838af47242bb5a89ff5ea9e8
pge1414 <101663790+pge1414@users.noreply.github.com>
2023-12-19 19:06:58 +0100

    a

--- K_RadiusKlassifikator.py
+++ K_RadiusKlassifikator.py
@@ -1,4 +1,4 @@
-import csv, random
+import csv
 
 def euklidischer_abstand(p, q):
     a = 0
@@ -12,24 +12,20 @@ with open("iris.csv") as f:
     for zeile in csv_reader:
         datensatz.append([float(wert) for wert in zeile[:-1]]+ [zeile[-1]])
 
-rdm_zeile = random.randint(0, 149)
-
-test_datensatz = datensatz[rdm_zeile]
-trainings_datensatz = datensatz.remove(test_datensatz)
+test_datensatz = datensatz[:1]
+trainings_datensatz = datensatz[1:]
 
 k = 1
 
 abstände = []
-for zeile in list(trainings_datensatz):
+for zeile in trainings_datensatz:
     abstände.append([euklidischer_abstand(zeile[:-1], test_datensatz[0][:-1]), zeile[-1]])
 
 abstände.sort(key=lambda x: x[0])
 
 top_k_abstände = []
 
-vorhersage_Radius = (max([abstände[1] for abstände in list(filter(lambda x: x[0]<=k, abstände))], key=list(filter(lambda x: x[0]<=k, abstände)).count))
-
-print(vorhersage_Radius)
+print(max([abstände[1] for abstände in list(filter(lambda x: x[0]<=k, abstände))], key=list(filter(lambda x: x[0]<=k, abstände)).count))
 
 # for i in abstände:
 #     if i[0] <= k:
@@ -38,6 +34,3 @@ print(vorhersage_Radius)
 # vorhersage = max([abstände[1] for abstände in top_k_abstände], key=top_k_abstände.count)
 
 #print(vorhersage)
-
-if vorhersage_Radius == test_datensatz[4]:
-    print("Ja")

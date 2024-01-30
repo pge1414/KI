751f1d2af8719bdc0ed19c55f9fbd301693b958e
pge1414 <101663790+pge1414@users.noreply.github.com>
2024-01-30 19:05:16 +0100

    a

--- 2023_12_19_KRK.py
+++ 2023_12_19_KRK.py
@@ -1,4 +1,7 @@
-import csv, math, random, tqdm
+# Idee
+# Es entscheiden nun nicht mehr die k nächsten Nachbarn, sondern alle, die in einem bestimmten Radius zur Eingabe liegen
+
+import csv, math, random, tqdm, matplotlib.pyplot as plt
 
 # https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data
 
@@ -8,58 +11,82 @@ def abstand(zeile1, zeile2):
         n += (zeile1[i] - zeile2[i]) ** 2
     return math.sqrt(n)
 
+def gewichtungen(datensatz): 
+    for i in range(len(datensatz)): datensatz[i][0] *= 5; datensatz[i][3] *= 10
+    return datensatz
+
 datensatz = []
 with open("iris.csv") as f:
     csv_reader = csv.reader(f)
     for zeile in csv_reader:
         datensatz.append([float(wert) for wert in zeile[:-1]] + [zeile[-1]])
-#print(datensatz)
-        
-# def krk(zeile : list, gesamt : list):
-#     primelist = []
-#     k = 0.5
-#     if abstand(zeile, gesamt) <= k:
-#         primelist.append(gesamt[4])
-#     return max(primelist.count("Iris-versicolor"), primelist.count("Iris-virginica"), primelist.count("Iris-setosa"))
+        gewichtungen(datensatz)
+# print(datensatz)
 
-def krk(testdatenzeile,trainingsdaten, k):
-    abstände = []
-
-
+# Transponiert datensatz, d.h. es werden Zeilen und Spalten vertauscht
+# *datensatz entpackt datensatz, d.h. statt einer Liste von Listen stehen dort nun viele verschiedene, voneinander getrennte Listen
+# zip fasst alle Einträge dieser Listen (150 Stück, da es 150 Einträge im Datensatz gibt) an der gleichen Stelle zu einem einzelnen Tupel zusammen
+# Also werden alle ersten Einträge zu einem Tupel zusammengefasst, alle zweiten, alle dritten, alle vierten und alle fünften.
+# Mittels list wird das so entstandene Objekt wieder zu einer gewöhnlichen Liste gemacht
 datensatz_transponiert = list(zip(*datensatz)) # ???
+datensatz_transponiert
+# Wir speichern die Maxima aller Spalten bis auf der mit den Blumennamen ab
 maxima = [max(datensatz_transponiert[0]), max(datensatz_transponiert[1]), max(datensatz_transponiert[2]), max(datensatz_transponiert[3])]
+# Wir gehen die Indizes aller Zeilen durch
 for i in range(len(datensatz)):
+    # Wir gehen die Indizes aller Spalten bis auf der letzten durch
     for j in range(len(datensatz[i]) - 1):
+        # Wir teilen den Eintrag durch das Maximum seiner Spalte
         datensatz[i][j] /= maxima[j]
 
-def validierung(p: float) -> float:
-    random.shuffle(datensatz)
-    testdaten = datensatz[:int(len(datensatz)*p)]
-    trainingsdaten = datensatz[int(len(datensatz)*p):]
 
+def krk(testdatenzeile, trainingsdaten, k):
+    abstände = []
+    for zeile in trainingsdaten:
+        abstände.append([abstand(zeile[:-1], testdatenzeile), zeile[-1]])
+
+    radius_k = list(filter(lambda x: x[0] <= k, abstände))
+
+    if not radius_k:
+        return ""
+
+    vorhersage = max([abstand[1] for abstand in radius_k], key=radius_k.count)
+    return vorhersage
+
+# Gibt den Prozentsatz der richtig klassifizierten Blumen zurück (für ein festgelegtes k)
+def validierung(p: float, k: float, h: int) -> float:
     treffer = 0
-    for testdatenzeile in testdaten:
-        vermutung = krk(testdatenzeile[:-1], trainingsdaten)
-        if vermutung == testdatenzeile[:-1]:
-            treffer += 1
     
-    return treffer / len(testdaten)
+    for _ in range(h):
+        random.shuffle(datensatz)
+        testdaten = datensatz[:int(len(datensatz) * p)] # Von Anfang an
+        trainingsdaten = datensatz[int(len(datensatz) * p):] # Bis zum Ende
 
-for k in tqdm.tqdm(range(10000)):
-    print(validierung(0.2))
+        for testdatenzeile in testdaten:
+            vermutung = krk(testdatenzeile[:-1], trainingsdaten, k) # krk bestimmt die Vorhersage, welcher Art testdatenzeile angehört
+            if vermutung == testdatenzeile[-1]:
+                treffer += 1
 
-# test_datensatz = datensatz[:1]
+    return treffer / (len(testdaten) * h)
 
-# trainings_datensatz = datensatz[1:]
+def k_optimierung(n: int):
+    ks = []
+    ergebnisse = []
+    k_opt = 0
+    erg_opt = 0
+    for k in tqdm.tqdm(range(n)):
+        ks.append(k / n)
+        erg = validierung(0.2, k / n, n // 2)
+        ergebnisse.append(erg)
+        if erg > erg_opt:
+            k_opt = k / n
+            erg_opt = erg
+    return ks, ergebnisse, k_opt, erg_opt
 
-# k = 0.2
-
-# abstände = []
-# for zeile in trainings_datensatz:
-#     abstände.append([abstand(zeile[:-1], test_datensatz[0][:-1]), zeile[-1]])
-# # print(abstände)
-
-# radius_k = list(filter(lambda x: x[0] <= k, abstände))
-
-# vorhersage = max([abstand[1] for abstand in radius_k], key=radius_k.count)
-
+d = k_optimierung(100)
+plt.plot(d[0], d[1])
+plt.scatter(d[2], d[3], c="red", s=100, label="Optimum")
+plt.xlabel("k")
+plt.ylabel("Genauigkeit")
+plt.title(f"Optimum bei k={d[2]}, μ={(round(d[3], 4))}")
+plt.show()

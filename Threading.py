b218ff785a1225811a644f366129e7c846c30c41
pge1414 <101663790+pge1414@users.noreply.github.com>
2024-02-27 18:59:43 +0100

    yup

--- Threading.py
+++ Threading.py
@@ -1,15 +1,41 @@
-# import threading
+import threading,time
 
-# fak = 0
+def summieren(liste, von, bis):
+    ergebnis = 0
+    for i in range(von, bis):
+        ergebnis += liste[i]
+    return ergebnis
 
-# def fakultät_multithreading(n, t):
+def sum_threads(liste, von, bis, ergebnisse, index):
+    summe = sum(liste[von:bis])
+    ergebnisse[index] = summe
 
-print(ord("s"))
+def listensumme_multithreaded(liste, k):
+    threads = []
+    ergebnisse = [1 for i in range(k)]
+    
+    for i in range(k):
+        t= threading.Thread(target=sum_threads, args= [liste, len(liste) // k * i, len(liste) // k * (i + 1), ergebnisse, i])
+        threads.append(t)
+        t.start()
 
-# a = threading.Thread(target=fak_helper1, args= (5,))
-# b = threading.Thread(target=fak_helper2, args = (5,))
-# a.start()
-# b.start()
-# a.join()
-# b.join()
-# print(fak)
+    ergebnis = 0
+
+    for i in range(k):
+        threads[i].join()
+        ergebnis += ergebnisse[i]
+
+    return ergebnis
+
+trainings_liste = []
+
+for i in range(200000000):    trainings_liste.append(i)
+
+start = time.time()
+
+print(listensumme_multithreaded(trainings_liste, 8))
+print(time.time() - start)
+
+start2 = time.time()
+print(sum(trainings_liste))
+print(time.time() - start2)

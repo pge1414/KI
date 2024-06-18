5d5eedb09fe8f109df6d752a9ea76a9f0e6e9e9d
pge1414 <101663790+pge1414@users.noreply.github.com>
2024-06-18 18:14:16 +0200

    aa

--- regressionsgerade.py
+++ regressionsgerade.py
@@ -1,3 +1,5 @@
+import csv, matplotlib.pyplot as plt
+
 datensatz = []
 with open("iris.csv") as f:
     csv_reader = csv.reader(f)
@@ -17,8 +19,17 @@ for i in datensatz:
     if i[4] == "Iris-setosa":
         x_setosa.append(i[0])
     if i[4] == "Iris-setosa":
-        y_setosa.append(i[01])
+        y_setosa.append(i[1])
 
-print(term(m(x_setosa, y_setosa), )
-      
-#6.0,2.2,5.0,1.5,Iris-virginica
+print(term(m(x_setosa, y_setosa), 5.4))
+
+################################################################
+
+################################################################
+
+# for i in range(x_setosa):
+#     term(m(x_setosa, y_setosa), x_setosa[i])
+
+#5.4,3.7,1.5,0.2,Iris-setosa
+#4.8,3.0,1.4,0.1,Iris-setosa
+#5.0,3.3,1.4,0.2,Iris-setosa

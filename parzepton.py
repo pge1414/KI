fab198bbc81dc11fc234962b25e12cd3003521eb
pge1414 <101663790+pge1414@users.noreply.github.com>
2024-11-26 19:00:02 +0100

    ..

---
+++ parzepton.py
@@ -0,0 +1,11 @@
+def Not(x : int):
+    return x - x + 1
+
+def And(x : int, y : int):
+    return x * y
+
+def AndII(x : int, y : int):
+    return x + y - 1
+
+def Or(x : int, y : int):
+    return x + y

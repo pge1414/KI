5df224ed9d4a856a251da7bf9391e8390cd3d26a
pge1414 <101663790+pge1414@users.noreply.github.com>
2024-12-10 19:01:23 +0100

    aaa

--- parzepton.py
+++ parzepton.py
@@ -6,8 +6,24 @@ def And(x : int, y : int):
     return x * y
 
 def AndII(x : int, y : int):
-    return bool(x + y - 1)
+    return x + y - 1
 
 def Or(x : int, y : int):
     return x + y
 
+def and_lernen():
+    x1g = 0
+    x2g = 0
+    x = [[0,0],[1,0],[0,1],[1,1]]
+    while True:
+        for i in x:
+            if x1g*i[0] + x2g*i[1] == AndII(i[0], i[1]):
+                return x1g, x2g
+            if x1g*i[0] + x2g*i[1] < AndII(i[0], i[1]):
+                x1g += 0.1
+                x2g += 0.1
+            if x1g*i[0] + x2g*i[1] > AndII(i[0], i[1]):
+                x1g -= 0.01
+                x2g -= 0.01
+
+print(and_lernen())

4f77730a2e5d6ec6a545c424343667533ab79d25
pge1414 <101663790+pge1414@users.noreply.github.com>
2025-01-07 18:57:44 +0100

    ss

--- parzepton.py
+++ parzepton.py
@@ -11,30 +11,53 @@ def AndII(x : int, y : int):
 def Or(x : int, y : int):
     return x + y
 
-def and_lernen():
-    x = [[0,0],[1,0],[0,1],[1,1]]
-    erg = [0,0]
-    x1g = erg[0]
-    x2g = erg[1]
-    while True:
-        for i in x:
-            if x1g*i[0] + x2g*i[1]-1 == AndII(i[0], i[1]):
-                True
-            if x1g*i[0] + x2g*i[1]-1 < AndII(i[0], i[1]):
-                x1g += 0.1
-                x2g += 0.1
-                print("a")
-            if x1g*i[0] + x2g*i[1]-1 > AndII(i[0], i[1]):
-                x1g -= 0.01
-                x2g -= 0.01
-            erg[0] = x1g
-            erg[1] = x2g
-            print(x1g, x2g)
-            a = [x1g*x[0][0] + x2g*x[0][1]-1, x1g*x[1][0] + x2g*x[1][1]-1, x1g*x[2][0] + x2g*x[2][1]-1, x1g*x[3][0] + x2g*x[3][1]-1]
-            b = [AndII(x[0][0],x[0][1]),AndII(x[1][0],x[1][1]),AndII(x[2][0],x[2][1]),AndII(x[3][0],x[3][1])]
-            if a == b:
-                False
-                return x1g, x2g
+# def and_lernen():
+#     x = [[0,0],[1,0],[0,1],[1,1]]
+#     erg = [0,0]
+#     x1g = erg[0]
+#     x2g = erg[1]
+#     while True:
+#         for i in x:
+#             if x1g*i[0] + x2g*i[1]-1 == AndII(i[0], i[1]):
+#                 True
+#             if x1g*i[0] + x2g*i[1]-1 < AndII(i[0], i[1]):
+#                 x1g += 0.1
+#                 x2g += 0.1
+#                 print("a")
+#             if x1g*i[0] + x2g*i[1]-1 > AndII(i[0], i[1]):
+#                 x1g -= 0.01
+#                 x2g -= 0.01
+#             erg[0] = x1g
+#             erg[1] = x2g
+#             print(x1g, x2g)
+#             a = [x1g*x[0][0] + x2g*x[0][1]-1, x1g*x[1][0] + x2g*x[1][1]-1, x1g*x[2][0] + x2g*x[2][1]-1, x1g*x[3][0] + x2g*x[3][1]-1]
+#             b = [AndII(x[0][0],x[0][1]),AndII(x[1][0],x[1][1]),AndII(x[2][0],x[2][1]),AndII(x[3][0],x[3][1])]
+#             if a == b:
+#                 False
+#                 return x1g, x2g
 
 
-print(and_lernen())
+# print(and_lernen())
+
+def classification(v,w,x,y):
+    if v*x + w*y <= 0:
+        return 0
+    else:
+        return 1
+
+def perceptronlearning(daten : list):
+    w = 0
+    v = 0
+    lernrate = 0.1
+    for a in range(10000):
+        for i in range(len(daten)-1):
+            x = daten[i][0]
+            y = daten[i][1]
+            if classification(v,w,x,y) != daten[i][2]:
+                w += lernrate    
+                v += lernrate
+                print(w,v)
+            else: 
+                if i == len(daten)-1:
+                    return w, v
+print (perceptronlearning([[1,1,1],[0,1,0]]))

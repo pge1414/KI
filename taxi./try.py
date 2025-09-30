d98f3b7a2d219900a77c1a145db1e5aebb950469
pge1414 <101663790+pge1414@users.noreply.github.com>
2025-09-30 18:59:45 +0200

    nn

--- taxi./try.py
+++ taxi./try.py
@@ -1,28 +1,33 @@
-import random
+# import random
 
-spielfeld = [["1",2,3,4,5],[1,2,"2",4,5],[1,2,3,4,5],[1,2,3,4,"3"],[1,"4",3,4,5]]
+# spielfeld = [["1",2,3,4,5],[1,2,"2",4,5],[1,2,3,4,5],[1,2,3,4,"3"],[1,"4",3,4,5]]
 
-def finder(voc : str, liste : list) -> list:
-    for i in range(len(liste)):
-        for j in range(len(liste[i])):
-            if liste[i][j] == voc:
-                return [i, j]
+# def finder(voc : str, liste : list) -> list:
+#     for i in range(len(liste)):
+#         for j in range(len(liste[i])):
+#             if liste[i][j] == voc:
+#                 return [i, j]
 
-def punktverteilung(liste : list) -> list:
-    punkt_passagier = str(random.randint(1,4))
-    punkt_ziel = str(random.randint(1,4))
-    if punkt_passagier != punkt_ziel:
-        pas = finder(punkt_passagier, liste)
-        ziel = finder(punkt_ziel, liste)
-        liste[pas[0]][pas[1]] = "p"
-        liste[ziel[0]][ziel[1]] = "z"
-    else:
-        punktverteilung(liste)
+# def punktverteilung(liste : list) -> list:
+#     punkt_passagier = str(random.randint(1,4))
+#     punkt_ziel = str(random.randint(1,4))
+#     if punkt_passagier != punkt_ziel:
+#         pas = finder(punkt_passagier, liste)
+#         ziel = finder(punkt_ziel, liste)
+#         liste[pas[0]][pas[1]] = "p"
+#         liste[ziel[0]][ziel[1]] = "z"
+#     else:
+#         punktverteilung(liste)
 
-punktverteilung(spielfeld)
-print(spielfeld)
+# punktverteilung(spielfeld)
+# print(spielfeld)
 
 # for i in range(len(spielfeld)):
 #     for j in range(len(spielfeld[i])):
 #         if spielfeld[i][j] == "4":
 #             print(i , j)
+liste = []
+for i in range(500):
+    liste.append(i)
+
+print(liste)
